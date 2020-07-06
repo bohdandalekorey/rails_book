806bbca034aac78b258c2503123efb3df7a0fb2e
bakitheme <gersi00057@gmail.com>
2020-07-06 16:48:34 +0300

    styeguide fixed

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -4,6 +4,7 @@ class MicropostsController < ApplicationController
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
+
     if @micropost.save
       flash[:success] = 'Micropost created!'
       redirect_to root_url
@@ -21,12 +22,12 @@ class MicropostsController < ApplicationController
 
   private
 
-    def micropost_params
-      params.require(:micropost).permit(:content, :picture)
-    end
+  def micropost_params
+    params.require(:micropost).permit(:content, :picture)
+  end
 
-    def correct_user
-      @micropost = current_user.microposts.find_by(id: params[:id])
-      redirect_to root_url if @micropost.nil?
-    end
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to root_url if @micropost.nil?
+  end
 end
